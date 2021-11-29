6af973226bcb1c33506404e3f3038c73df3d5263
Chukwu Matthew <matthewnfvcb@gmail.com>
2021-11-29 19:39:06 +0100

    updated

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
         default: {
             connector: 'mongoose',
             settings: {
-                host: env('DATABASE_HOST', ),
+                host: env('DATABASE_HOST', 'cluster1.9vjt4.mongodb.net'),
                 srv: env.bool('DATABASE_SRV', true),
                 port: env.int('DATABASE_PORT', 27017),
-                database: env('DATABASE_NAME', ),
-                username: env('DATABASE_USERNAME', ),
-                password: env('DATABASE_PASSWORD', ),
+                database: env('DATABASE_NAME', 'portfolio'),
+                username: env('DATABASE_USERNAME', 'Matthew'),
+                password: env('DATABASE_PASSWORD', 'chukwumc'),
             },
             options: {
                 authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
